1f9dcac05bfe8ade5ecfd3d17810d64f50e18443
Pavel Sušický <me@frontendista.cz>
2025-08-12 16:49:51 +0200

    feat: externalize configuration

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,19 +4,25 @@ import { AppService } from "./app.service";
 import { TypeOrmModule } from "@nestjs/typeorm";
 import { SnippetModule } from "./snippet/snippet.module";
 import { Snippet } from "./domain/snippet";
+import { ConfigModule, ConfigService } from "@nestjs/config";
 
 @Module({
 	imports: [
+		ConfigModule.forRoot({ isGlobal: true }),
 		SnippetModule,
-		TypeOrmModule.forRoot({
-			type: "postgres",
-			host: "localhost",
-			port: 5432,
-			username: "tereza",
-			password: "cervenytrakturek",
-			database: "swa",
-			entities: [Snippet],
-			synchronize: true
+		TypeOrmModule.forRootAsync({
+			imports: [ConfigModule],
+			inject: [ConfigService],
+			useFactory: (configService: ConfigService) => ({
+				type: "postgres",
+				host: configService.get<string>("POSTGRES_HOST"),
+				port: 5432,
+				username: configService.get<string>("POSTGRES_USERNAME"),
+				password: configService.get<string>("POSTGRES_PASSWORD"),
+				database: "swa",
+				entities: [Snippet],
+				synchronize: true
+			})
 		})
 	],
 	controllers: [AppController],
