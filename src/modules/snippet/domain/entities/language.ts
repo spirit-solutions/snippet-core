c15415c81eb6bfa35f7e35410518216061672942
Pavel Sušický <me@frontendista.cz>
2025-08-19 18:13:15 +0200

    refactor

--- src/modules/snippet/domain/entities/language.ts
+++ src/modules/snippet/domain/entities/language.ts
@@ -2,17 +2,6 @@ import { HttpStatus } from "@nestjs/common";
 import { RpcException } from "@nestjs/microservices";
 import z from "zod";
 
-export const languageSchema = z.custom<Language>(
-	val => val instanceof Language,
-	{
-		message: "Input is not an instance of Language"
-	}
-);
-
-export const SUPPORTED_LANGUAGES = ["javascript", "python", "typescript"] as const;
-
-export type SupportedLanguages = typeof SUPPORTED_LANGUAGES[number];
-
 export class Language {
 	private constructor(public readonly value: SupportedLanguages) {
 		if (!SUPPORTED_LANGUAGES.includes(value)) {
@@ -24,6 +13,28 @@ export class Language {
 	}
 
 	public static create(value: SupportedLanguages): Language {
+		const validationResult = language.safeParse(value);
+
+		if (!validationResult.success) {
+			const errorMessages = validationResult.error.issues.map(issue => issue.message).join("; ");
+			throw new RpcException({
+				status: HttpStatus.BAD_REQUEST,
+				message: errorMessages
+			});
+		}
+
 		return new Language(value);
 	}
 }
+
+export const SUPPORTED_LANGUAGES = ["javascript", "python", "typescript"] as const;
+export const language = z.enum(SUPPORTED_LANGUAGES);
+
+export const languageSchema = z.custom<Language>(
+	val => val instanceof Language,
+	{
+		message: "Input is not an instance of Language"
+	}
+);
+
+export type SupportedLanguages = typeof SUPPORTED_LANGUAGES[number];
